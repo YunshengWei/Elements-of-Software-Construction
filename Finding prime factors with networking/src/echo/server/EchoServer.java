b8c93772f7642c9489c5c130469e87bcbd420843
Yunsheng Wei <weiyunshengwei@gmail.com>
2014-07-13 15:59:43 +0800

    Finished EchoServer

--- Finding prime factors with networking/src/echo/server/EchoServer.java
+++ Finding prime factors with networking/src/echo/server/EchoServer.java
@@ -1,19 +1,50 @@
 package echo.server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 
 /**
  * A simple server that will echo client inputs.
  */
 public class EchoServer {
-    	
+
     /**
-     * @param args String array containing Program arguments.  It should only 
-     *      contain at most one String indicating the port it should connect to.
-     *      The String should be parseable into an int.  
-     *      If no arguments, we default to port 4444.
+     * @param args
+     *            String array containing Program arguments. It should only
+     *            contain at most one String indicating the port it should
+     *            connect to. The String should be parseable into an int. If no
+     *            arguments, we default to port 4444.
      */
-	public static void main(String[] args) throws IOException {
-	    //TODO complete this implementation.
+    public static void main(String[] args) throws IOException {
+        //TODO complete this implementation.
+        int port = 4444;
+        if (args.length > 1) {
+            System.err.println("There should be at most one argument.");
+            System.exit(1);
+        } else if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+            while (true) {
+                try (
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                    ) {
+                    String inputline;
+                    while ((inputline = in.readLine()) != null) {
+                        out.println(inputline);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
     }
 }
